2820cbc9a4a4dc1df4e08fda1a65495be5f2a9d3
artur87a <artur.kodehode@gmail.com>
2024-04-12 15:55:58 +0200

    12.04.2024

--- js/script.js
+++ js/script.js
@@ -18,8 +18,27 @@ const handleCurrentYear = () => {
   footerYear.innerText = year;
 };
 handleCurrentYear();
-navBtn.addEventListener("click", handleNav);
+// navBtn.addEventListener("click", handleNav);
+//////////////////////////////
+//////////buttonTOP//////////
+/////////////////////////////
+const mybutton = document.getElementById("myBtnTop");
+mybutton.addEventListener("click", topFunction);
+window.onscroll = function () {
+  scrollFunction();
+};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 // document.addEventListener("DOMContentLoaded", function () {
 //   const dropdownToggleButtons = document.querySelectorAll(
 //     ".price-menu .dropdown-toggle"
